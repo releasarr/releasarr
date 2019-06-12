quickstar/Radarr

New: Backend changes for new UI

src/Radarr.Api.V2/Config/MediaManagementConfigResource.cs
@@ -1,0 +1,58 @@
+using Radarr.Http.REST;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.MediaFiles;
+
+namespace Radarr.Api.V2.Config
+{
+    public class MediaManagementConfigResource : RestResource
+    {
+        public bool AutoUnmonitorPreviouslyDownloadedEpisodes { get; set; }
+        public string RecycleBin { get; set; }
+        public bool AutoDownloadPropers { get; set; }
+        public bool CreateEmptySeriesFolders { get; set; }
+        public FileDateType FileDate { get; set; }
+        public bool AutoRenameFolders { get; set; }
+        public bool PathsDefaultStatic { get; set; }
+
+        public bool SetPermissionsLinux { get; set; }
+        public string FileChmod { get; set; }
+        public string FolderChmod { get; set; }
+        public string ChownUser { get; set; }
+        public string ChownGroup { get; set; }
+
+        public bool SkipFreeSpaceCheckWhenImporting { get; set; }
+        public bool CopyUsingHardlinks { get; set; }
+        public bool ImportExtraFiles { get; set; }
+        public string ExtraFileExtensions { get; set; }
+        public bool EnableMediaInfo { get; set; }
+    }
+
+    public static class MediaManagementConfigResourceMapper
+    {
+        public static MediaManagementConfigResource ToResource(IConfigService model)
+        {
+            return new MediaManagementConfigResource
+            {
+                AutoUnmonitorPreviouslyDownloadedEpisodes = model.AutoUnmonitorPreviouslyDownloadedEpisodes,
+                RecycleBin = model.RecycleBin,
+                AutoDownloadPropers = model.AutoDownloadPropers,
+                CreateEmptySeriesFolders = model.CreateEmptySeriesFolders,
+                FileDate = model.FileDate,
+                AutoRenameFolders = model.AutoRenameFolders,
+                PathsDefaultStatic = model.PathsDefaultStatic,
+
+                SetPermissionsLinux = model.SetPermissionsLinux,
+                FileChmod = model.FileChmod,
+                FolderChmod = model.FolderChmod,
+                ChownUser = model.ChownUser,
+                ChownGroup = model.ChownGroup,
+
+                SkipFreeSpaceCheckWhenImporting = model.SkipFreeSpaceCheckWhenImporting,
+                CopyUsingHardlinks = model.CopyUsingHardlinks,
+                ImportExtraFiles = model.ImportExtraFiles,
+                ExtraFileExtensions = model.ExtraFileExtensions,
+                EnableMediaInfo = model.EnableMediaInfo
+            };
+        }
+    }
+}

src/Radarr.Api.V2/MovieFiles/MovieFileModule.cs
@@ -1,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy;
+using NLog;
+using NzbDrone.Core.Datastore.Events;
+using NzbDrone.Core.DecisionEngine;
+using NzbDrone.Core.Exceptions;
+using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.MediaFiles.Events;
+using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Movies;
+using NzbDrone.SignalR;
+using Radarr.Http;
+using Radarr.Http.Extensions;
+using BadRequestException = Radarr.Http.REST.BadRequestException;
+
+namespace Radarr.Api.V2.MovieFiles
+{
+    public class MovieFileModule : RadarrRestModuleWithSignalR<MovieFileResource, MovieFile>,
+                                 IHandle<MovieFileAddedEvent>,
+                                 IHandle<MovieFileDeletedEvent>
+    {
+        private readonly IMediaFileService _mediaFileService;
+        private readonly IRecycleBinProvider _recycleBinProvider;
+        private readonly IMovieService _movieService;
+        private readonly IQualityUpgradableSpecification _qualityUpgradableSpecification;
+        private readonly Logger _logger;
+
+        public MovieFileModule(IBroadcastSignalRMessage signalRBroadcaster,
+                             IMediaFileService mediaFileService,
+                             IMovieService movieService,
+                             IQualityUpgradableSpecification qualityUpgradableSpecification,
+                             Logger logger)
+            : base(signalRBroadcaster)
+        {
+            _mediaFileService = mediaFileService;
+            _movieService = movieService;
+            _qualityUpgradableSpecification = qualityUpgradableSpecification;
+            _logger = logger;
+
+            GetResourceById = GetMovieFile;
+            GetResourceAll = GetMovieFiles;
+            UpdateResource = SetQuality;
+            DeleteResource = DeleteMovieFile;
+
+            Put["/editor"] = movieFiles => SetQuality();
+            Delete["/bulk"] = movieFiles => DeleteMovieFiles();
+        }
+
+        private MovieFileResource GetMovieFile(int id)
+        {
+            var movieFile = _mediaFileService.GetMovie(id);
+            var movie = _movieService.GetMovie(movieFile.MovieId);
+
+            return movieFile.ToResource(movie, _qualityUpgradableSpecification);
+        }
+
+        private List<MovieFileResource> GetMovieFiles()
+        {
+            var movieIdQuery = Request.Query.SeriesId;
+            var movieFileIdsQuery = Request.Query.MovieFileIds;
+
+            if (!movieIdQuery.HasValue && !movieFileIdsQuery.HasValue)
+            {
+                throw new BadRequestException("movieId or movieFileIds must be provided");
+            }
+
+            if (movieIdQuery.HasValue)
+            {
+                int movieId = Convert.ToInt32(movieIdQuery.Value);
+                var movie = _movieService.GetMovie(movieId);
+
+                return _mediaFileService.GetFilesByMovie(movieId).ConvertAll(f => f.ToResource(movie, _qualityUpgradableSpecification));
+            }
+
+            else
+            {
+                string movieFileIdsValue = movieFileIdsQuery.Value.ToString();
+
+                var movieFileIds = movieFileIdsValue.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                                        .Select(e => Convert.ToInt32(e))
+                                                        .ToList();
+
+                var movieFiles = _mediaFileService.GetMovies(movieFileIds);
+
+                return movieFiles.GroupBy(e => e.MovieId)
+                                   .SelectMany(f => f.ToList()
+                                                     .ConvertAll( e => e.ToResource(_movieService.GetMovie(f.Key), _qualityUpgradableSpecification)))
+                                   .ToList();
+            }
+        }
+
+        private void SetQuality(MovieFileResource movieFileResource)
+        {
+            var movieFile = _mediaFileService.GetMovie(movieFileResource.Id);
+            movieFile.Quality = movieFileResource.Quality;
+            _mediaFileService.Update(movieFile);
+        }
+
+        private Response SetQuality()
+        {
+            var resource = Request.Body.FromJson<MovieFileListResource>();
+            var movieFiles = _mediaFileService.GetMovies(resource.MovieFileIds);
+
+            foreach (var movieFile in movieFiles)
+            {
+
+                if (resource.Quality != null)
+                {
+                    movieFile.Quality = resource.Quality;
+                }
+            }
+
+            _mediaFileService.Update(movieFiles);
+
+            var movie = _movieService.GetMovie(movieFiles.First().MovieId);
+
+            return movieFiles.ConvertAll(f => f.ToResource(movie, _qualityUpgradableSpecification))
+                               .AsResponse(HttpStatusCode.Accepted);
+        }
+
+        private void DeleteMovieFile(int id)
+        {
+            var movieFile = _mediaFileService.GetMovie(id);
+            var movie = _movieService.GetMovie(movieFile.MovieId);
+            var fullPath = Path.Combine(movie.Path, movieFile.RelativePath);
+
+            _logger.Info("Deleting movie file: {0}", fullPath);
+            _recycleBinProvider.DeleteFile(fullPath);
+            _mediaFileService.Delete(movieFile, DeleteMediaFileReason.Manual);
+            // TODO: Pull MediaFileDeletionService from Sonarr
+            //_mediaFileDeletionService.Delete(series, episodeFile);
+        }
+
+        private Response DeleteMovieFiles()
+        {
+            var resource = Request.Body.FromJson<MovieFileListResource>();
+            var movieFiles = _mediaFileService.GetMovies(resource.MovieFileIds);
+            var movie = _movieService.GetMovie(movieFiles.First().MovieId);
+
+            foreach (var movieFile in movieFiles)
+            {
+                var fullPath = Path.Combine(movie.Path, movieFile.RelativePath);
+                _logger.Info("Deleting movie file: {0}", fullPath);
+                _recycleBinProvider.DeleteFile(fullPath);
+                _mediaFileService.Delete(movieFile, DeleteMediaFileReason.Manual);
+                // TODO: Pull MediaFileDeletionService from Sonarr
+                //_mediaFileDeletionService.DeleteEpisodeFile(movie, movieFile);
+            }
+
+            return new object().AsResponse();
+        }
+
+        public void Handle(MovieFileAddedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Updated, message.MovieFile.Id);
+        }
+
+        public void Handle(MovieFileDeletedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Deleted, message.MovieFile.Id);
+        }
+    }
+}

releasarr/releasarr

src/Radarr.Api.V2/Config/MediaManagementConfigResource.cs
@@ -1,0 +1,62 @@
+using Radarr.Http.REST;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.MediaFiles;
+
+namespace Radarr.Api.V2.Config
+{
+    public class MediaManagementConfigResource : RestResource
+    {
+        public bool AutoUnmonitorPreviouslyDownloadedMovies { get; set; }
+        public string RecycleBin { get; set; }
+        public bool AutoDownloadPropers { get; set; }
+        public bool CreateEmptyMovieFolders { get; set; }
+        public bool DeleteEmptyFolders { get; set; }
+        public FileDateType FileDate { get; set; }
+        public RescanAfterRefreshType RescanAfterRefresh { get; set; }
+        public bool AutoRenameFolders { get; set; }
+        public bool PathsDefaultStatic { get; set; }
+
+        public bool SetPermissionsLinux { get; set; }
+        public string FileChmod { get; set; }
+        public string FolderChmod { get; set; }
+        public string ChownUser { get; set; }
+        public string ChownGroup { get; set; }
+
+        public bool SkipFreeSpaceCheckWhenImporting { get; set; }
+        public bool CopyUsingHardlinks { get; set; }
+        public bool ImportExtraFiles { get; set; }
+        public string ExtraFileExtensions { get; set; }
+        public bool EnableMediaInfo { get; set; }
+    }
+
+    public static class MediaManagementConfigResourceMapper
+    {
+        public static MediaManagementConfigResource ToResource(IConfigService model)
+        {
+            return new MediaManagementConfigResource
+            {
+                AutoUnmonitorPreviouslyDownloadedMovies = model.AutoUnmonitorPreviouslyDownloadedMovies,
+                RecycleBin = model.RecycleBin,
+                AutoDownloadPropers = model.AutoDownloadPropers,
+                CreateEmptyMovieFolders = model.CreateEmptyMovieFolders,
+                DeleteEmptyFolders = model.DeleteEmptyFolders,
+                FileDate = model.FileDate,
+                RescanAfterRefresh = model.RescanAfterRefresh,
+                AutoRenameFolders = model.AutoRenameFolders,
+                PathsDefaultStatic = model.PathsDefaultStatic,
+
+                SetPermissionsLinux = model.SetPermissionsLinux,
+                FileChmod = model.FileChmod,
+                FolderChmod = model.FolderChmod,
+                ChownUser = model.ChownUser,
+                ChownGroup = model.ChownGroup,
+
+                SkipFreeSpaceCheckWhenImporting = model.SkipFreeSpaceCheckWhenImporting,
+                CopyUsingHardlinks = model.CopyUsingHardlinks,
+                ImportExtraFiles = model.ImportExtraFiles,
+                ExtraFileExtensions = model.ExtraFileExtensions,
+                EnableMediaInfo = model.EnableMediaInfo
+            };
+        }
+    }
+}

src/Radarr.Api.V2/MovieFiles/MovieFileModule.cs
@@ -1,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy;
+using NLog;
+using NzbDrone.Core.Datastore.Events;
+using NzbDrone.Core.DecisionEngine;
+using NzbDrone.Core.Exceptions;
+using NzbDrone.Core.MediaFiles;
+using NzbDrone.Core.MediaFiles.Events;
+using NzbDrone.Core.Messaging.Events;
+using NzbDrone.Core.Movies;
+using NzbDrone.SignalR;
+using Radarr.Http;
+using Radarr.Http.Extensions;
+using BadRequestException = Radarr.Http.REST.BadRequestException;
+
+namespace Radarr.Api.V2.MovieFiles
+{
+    public class MovieFileModule : RadarrRestModuleWithSignalR<MovieFileResource, MovieFile>,
+                                 IHandle<MovieFileAddedEvent>,
+                                 IHandle<MovieFileDeletedEvent>
+    {
+        private readonly IMediaFileService _mediaFileService;
+        private readonly IRecycleBinProvider _recycleBinProvider;
+        private readonly IMovieService _movieService;
+        private readonly IQualityUpgradableSpecification _qualityUpgradableSpecification;
+        private readonly Logger _logger;
+
+        public MovieFileModule(IBroadcastSignalRMessage signalRBroadcaster,
+                             IMediaFileService mediaFileService,
+                             IMovieService movieService,
+                             IQualityUpgradableSpecification qualityUpgradableSpecification,
+                             Logger logger)
+            : base(signalRBroadcaster)
+        {
+            _mediaFileService = mediaFileService;
+            _movieService = movieService;
+            _qualityUpgradableSpecification = qualityUpgradableSpecification;
+            _logger = logger;
+
+            GetResourceById = GetMovieFile;
+            GetResourceAll = GetMovieFiles;
+            UpdateResource = SetQuality;
+            DeleteResource = DeleteMovieFile;
+
+            Put["/editor"] = movieFiles => SetQuality();
+            Delete["/bulk"] = movieFiles => DeleteMovieFiles();
+        }
+
+        private MovieFileResource GetMovieFile(int id)
+        {
+            var movieFile = _mediaFileService.GetMovie(id);
+            var movie = _movieService.GetMovie(movieFile.MovieId);
+
+            return movieFile.ToResource(movie, _qualityUpgradableSpecification);
+        }
+
+        private List<MovieFileResource> GetMovieFiles()
+        {
+            var movieIdQuery = Request.Query.MovieId;
+            var movieFileIdsQuery = Request.Query.MovieFileIds;
+
+            if (!movieIdQuery.HasValue && !movieFileIdsQuery.HasValue)
+            {
+                throw new BadRequestException("movieId or movieFileIds must be provided");
+            }
+
+            if (movieIdQuery.HasValue)
+            {
+                int movieId = Convert.ToInt32(movieIdQuery.Value);
+                var movie = _movieService.GetMovie(movieId);
+
+                return _mediaFileService.GetFilesByMovie(movieId).ConvertAll(f => f.ToResource(movie, _qualityUpgradableSpecification));
+            }
+
+            else
+            {
+                string movieFileIdsValue = movieFileIdsQuery.Value.ToString();
+
+                var movieFileIds = movieFileIdsValue.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                                        .Select(e => Convert.ToInt32(e))
+                                                        .ToList();
+
+                var movieFiles = _mediaFileService.GetMovies(movieFileIds);
+
+                return movieFiles.GroupBy(e => e.MovieId)
+                                   .SelectMany(f => f.ToList()
+                                                     .ConvertAll( e => e.ToResource(_movieService.GetMovie(f.Key), _qualityUpgradableSpecification)))
+                                   .ToList();
+            }
+        }
+
+        private void SetQuality(MovieFileResource movieFileResource)
+        {
+            var movieFile = _mediaFileService.GetMovie(movieFileResource.Id);
+            movieFile.Quality = movieFileResource.Quality;
+            _mediaFileService.Update(movieFile);
+        }
+
+        private Response SetQuality()
+        {
+            var resource = Request.Body.FromJson<MovieFileListResource>();
+            var movieFiles = _mediaFileService.GetMovies(resource.MovieFileIds);
+
+            foreach (var movieFile in movieFiles)
+            {
+
+                if (resource.Quality != null)
+                {
+                    movieFile.Quality = resource.Quality;
+                }
+            }
+
+            _mediaFileService.Update(movieFiles);
+
+            var movie = _movieService.GetMovie(movieFiles.First().MovieId);
+
+            return movieFiles.ConvertAll(f => f.ToResource(movie, _qualityUpgradableSpecification))
+                               .AsResponse(HttpStatusCode.Accepted);
+        }
+
+        private void DeleteMovieFile(int id)
+        {
+            var movieFile = _mediaFileService.GetMovie(id);
+            var movie = _movieService.GetMovie(movieFile.MovieId);
+            var fullPath = Path.Combine(movie.Path, movieFile.RelativePath);
+
+            _logger.Info("Deleting movie file: {0}", fullPath);
+            _recycleBinProvider.DeleteFile(fullPath);
+            _mediaFileService.Delete(movieFile, DeleteMediaFileReason.Manual);
+            // TODO: Pull MediaFileDeletionService from Sonarr
+            //_mediaFileDeletionService.Delete(series, episodeFile);
+        }
+
+        private Response DeleteMovieFiles()
+        {
+            var resource = Request.Body.FromJson<MovieFileListResource>();
+            var movieFiles = _mediaFileService.GetMovies(resource.MovieFileIds);
+            var movie = _movieService.GetMovie(movieFiles.First().MovieId);
+
+            foreach (var movieFile in movieFiles)
+            {
+                var fullPath = Path.Combine(movie.Path, movieFile.RelativePath);
+                _logger.Info("Deleting movie file: {0}", fullPath);
+                _recycleBinProvider.DeleteFile(fullPath);
+                _mediaFileService.Delete(movieFile, DeleteMediaFileReason.Manual);
+                // TODO: Pull MediaFileDeletionService from Sonarr
+                //_mediaFileDeletionService.DeleteEpisodeFile(movie, movieFile);
+            }
+
+            return new object().AsResponse();
+        }
+
+        public void Handle(MovieFileAddedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Updated, message.MovieFile.Id);
+        }
+
+        public void Handle(MovieFileDeletedEvent message)
+        {
+            BroadcastResourceChange(ModelAction.Deleted, message.MovieFile.Id);
+        }
+    }
+}