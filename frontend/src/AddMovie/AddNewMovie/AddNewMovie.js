quickstar/Radarr

New: Project Aphrodite

frontend/src/AddMovie/AddNewMovie/AddNewMovie.js
@@ -1,0 +1,182 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { icons } from 'Helpers/Props';
+import Button from 'Components/Link/Button';
+import Link from 'Components/Link/Link';
+import Icon from 'Components/Icon';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import TextInput from 'Components/Form/TextInput';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBodyConnector from 'Components/Page/PageContentBodyConnector';
+import AddNewMovieSearchResultConnector from './AddNewMovieSearchResultConnector';
+import styles from './AddNewMovie.css';
+
+class AddNewMovie extends Component {
+
+  //
+  // Lifecycle
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      term: props.term || '',
+      isFetching: false
+    };
+  }
+
+  componentDidMount() {
+    const term = this.state.term;
+
+    if (term) {
+      this.props.onMovieLookupChange(term);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      term,
+      isFetching
+    } = this.props;
+
+    if (term && term !== prevProps.term) {
+      this.setState({
+        term,
+        isFetching: true
+      });
+      this.props.onMovieLookupChange(term);
+    } else if (isFetching !== prevProps.isFetching) {
+      this.setState({
+        isFetching
+      });
+    }
+  }
+
+  //
+  // Listeners
+
+  onSearchInputChange = ({ value }) => {
+    const hasValue = !!value.trim();
+
+    this.setState({ term: value, isFetching: hasValue }, () => {
+      if (hasValue) {
+        this.props.onMovieLookupChange(value);
+      } else {
+        this.props.onClearMovieLookup();
+      }
+    });
+  }
+
+  onClearMovieLookupPress = () => {
+    this.setState({ term: '' });
+    this.props.onClearMovieLookup();
+  }
+
+  //
+  // Render
+
+  render() {
+    const {
+      error,
+      items
+    } = this.props;
+
+    const term = this.state.term;
+    const isFetching = this.state.isFetching;
+
+    return (
+      <PageContent title="Add New Movie">
+        <PageContentBodyConnector>
+          <div className={styles.searchContainer}>
+            <div className={styles.searchIconContainer}>
+              <Icon
+                name={icons.SEARCH}
+                size={20}
+              />
+            </div>
+
+            <TextInput
+              className={styles.searchInput}
+              name="movieLookup"
+              value={term}
+              placeholder="eg. Some Movie, tmdb:####, imdb:####"
+              onChange={this.onSearchInputChange}
+            />
+
+            <Button
+              className={styles.clearLookupButton}
+              onPress={this.onClearMovieLookupPress}
+            >
+              <Icon
+                name={icons.REMOVE}
+                size={20}
+              />
+            </Button>
+          </div>
+
+          {
+            isFetching &&
+              <LoadingIndicator />
+          }
+
+          {
+            !isFetching && !!error &&
+              <div>Failed to load search results, please try again.</div>
+          }
+
+          {
+            !isFetching && !error && !!items.length &&
+              <div className={styles.searchResults}>
+                {
+                  items.map((item) => {
+                    return (
+                      <AddNewMovieSearchResultConnector
+                        key={item.imdbId}
+                        {...item}
+                      />
+                    );
+                  })
+                }
+              </div>
+          }
+
+          {
+            !isFetching && !error && !items.length && !!term &&
+              <div className={styles.message}>
+                <div className={styles.noResults}>Couldn't find any results for '{term}'</div>
+                <div>You can also search using TMDB ID or IMDB ID of a show. eg. tmdb:71663</div>
+                <div>
+                  <Link to="https://github.com/Radarr/Radarr/wiki/FAQ#why-cant-i-add-a-new-series-when-i-know-the-tvdb-id">
+                    Why can't I find my movie?
+                  </Link>
+                </div>
+              </div>
+          }
+
+          {
+            !term &&
+              <div className={styles.message}>
+                <div className={styles.helpText}>It's easy to add a new movie, just start typing the name the movie you want to add.</div>
+                <div>You can also search using TMDB ID of a show. eg. tmdb:71663</div>
+              </div>
+          }
+
+          <div />
+        </PageContentBodyConnector>
+      </PageContent>
+    );
+  }
+}
+
+AddNewMovie.propTypes = {
+  term: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.object,
+  isAdding: PropTypes.bool.isRequired,
+  addError: PropTypes.object,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onMovieLookupChange: PropTypes.func.isRequired,
+  onClearMovieLookup: PropTypes.func.isRequired
+};
+
+export default AddNewMovie;

frontend/src/AddMovie/AddNewMovie/AddNewMovieModalContentConnector.js
@@ -1,0 +1,97 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import { setAddMovieDefault, addMovie } from 'Store/Actions/addMovieActions';
+import createDimensionsSelector from 'Store/Selectors/createDimensionsSelector';
+import selectSettings from 'Store/Selectors/selectSettings';
+import AddNewMovieModalContent from './AddNewMovieModalContent';
+
+function createMapStateToProps() {
+  return createSelector(
+    (state) => state.addMovie,
+    createDimensionsSelector(),
+    (addMovieState, dimensions) => {
+      const {
+        isAdding,
+        addError,
+        defaults
+      } = addMovieState;
+
+      const {
+        settings,
+        validationErrors,
+        validationWarnings
+      } = selectSettings(defaults, {}, addError);
+
+      return {
+        isAdding,
+        addError,
+        isSmallScreen: dimensions.isSmallScreen,
+        validationErrors,
+        validationWarnings,
+        ...settings
+      };
+    }
+  );
+}
+
+const mapDispatchToProps = {
+  setAddMovieDefault,
+  addMovie
+};
+
+class AddNewMovieModalContentConnector extends Component {
+
+  //
+  // Listeners
+
+  onInputChange = ({ name, value }) => {
+    this.props.setAddMovieDefault({ [name]: value });
+  }
+
+  onAddSeriesPress = (searchForMissingEpisodes) => {
+    const {
+      tvdbId,
+      rootFolderPath,
+      monitor,
+      qualityProfileId,
+      tags
+    } = this.props;
+
+    this.props.addMovie({
+      tvdbId,
+      rootFolderPath: rootFolderPath.value,
+      monitor: monitor.value,
+      qualityProfileId: qualityProfileId.value,
+      tags: tags.value,
+      searchForMissingEpisodes
+    });
+  }
+
+  //
+  // Render
+
+  render() {
+    return (
+      <AddNewMovieModalContent
+        {...this.props}
+        onInputChange={this.onInputChange}
+        onAddSeriesPress={this.onAddSeriesPress}
+      />
+    );
+  }
+}
+
+AddNewMovieModalContentConnector.propTypes = {
+  tvdbId: PropTypes.number.isRequired,
+  rootFolderPath: PropTypes.object,
+  monitor: PropTypes.object.isRequired,
+  qualityProfileId: PropTypes.object,
+  tags: PropTypes.object.isRequired,
+  onModalClose: PropTypes.func.isRequired,
+  setAddMovieDefault: PropTypes.func.isRequired,
+  addMovie: PropTypes.func.isRequired
+};
+
+export default connect(createMapStateToProps, mapDispatchToProps)(AddNewMovieModalContentConnector);

releasarr/releasarr

frontend/src/AddMovie/AddNewMovie/AddNewMovie.js
@@ -1,0 +1,182 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { icons } from 'Helpers/Props';
+import Button from 'Components/Link/Button';
+import Link from 'Components/Link/Link';
+import Icon from 'Components/Icon';
+import LoadingIndicator from 'Components/Loading/LoadingIndicator';
+import TextInput from 'Components/Form/TextInput';
+import PageContent from 'Components/Page/PageContent';
+import PageContentBodyConnector from 'Components/Page/PageContentBodyConnector';
+import AddNewMovieSearchResultConnector from './AddNewMovieSearchResultConnector';
+import styles from './AddNewMovie.css';
+
+class AddNewMovie extends Component {
+
+  //
+  // Lifecycle
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      term: props.term || '',
+      isFetching: false
+    };
+  }
+
+  componentDidMount() {
+    const term = this.state.term;
+
+    if (term) {
+      this.props.onMovieLookupChange(term);
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      term,
+      isFetching
+    } = this.props;
+
+    if (term && term !== prevProps.term) {
+      this.setState({
+        term,
+        isFetching: true
+      });
+      this.props.onMovieLookupChange(term);
+    } else if (isFetching !== prevProps.isFetching) {
+      this.setState({
+        isFetching
+      });
+    }
+  }
+
+  //
+  // Listeners
+
+  onSearchInputChange = ({ value }) => {
+    const hasValue = !!value.trim();
+
+    this.setState({ term: value, isFetching: hasValue }, () => {
+      if (hasValue) {
+        this.props.onMovieLookupChange(value);
+      } else {
+        this.props.onClearMovieLookup();
+      }
+    });
+  }
+
+  onClearMovieLookupPress = () => {
+    this.setState({ term: '' });
+    this.props.onClearMovieLookup();
+  }
+
+  //
+  // Render
+
+  render() {
+    const {
+      error,
+      items
+    } = this.props;
+
+    const term = this.state.term;
+    const isFetching = this.state.isFetching;
+
+    return (
+      <PageContent title="Add New Movie">
+        <PageContentBodyConnector>
+          <div className={styles.searchContainer}>
+            <div className={styles.searchIconContainer}>
+              <Icon
+                name={icons.SEARCH}
+                size={20}
+              />
+            </div>
+
+            <TextInput
+              className={styles.searchInput}
+              name="movieLookup"
+              value={term}
+              placeholder="eg. Some Movie, tmdb:####, imdb:####"
+              onChange={this.onSearchInputChange}
+            />
+
+            <Button
+              className={styles.clearLookupButton}
+              onPress={this.onClearMovieLookupPress}
+            >
+              <Icon
+                name={icons.REMOVE}
+                size={20}
+              />
+            </Button>
+          </div>
+
+          {
+            isFetching &&
+              <LoadingIndicator />
+          }
+
+          {
+            !isFetching && !!error &&
+              <div>Failed to load search results, please try again.</div>
+          }
+
+          {
+            !isFetching && !error && !!items.length &&
+              <div className={styles.searchResults}>
+                {
+                  items.map((item) => {
+                    return (
+                      <AddNewMovieSearchResultConnector
+                        key={item.tmdbId}
+                        {...item}
+                      />
+                    );
+                  })
+                }
+              </div>
+          }
+
+          {
+            !isFetching && !error && !items.length && !!term &&
+              <div className={styles.message}>
+                <div className={styles.noResults}>Couldn't find any results for '{term}'</div>
+                <div>You can also search using TMDB ID or IMDB ID of a movie. eg. tmdb:71663</div>
+                <div>
+                  <Link to="https://github.com/Radarr/Radarr/wiki/FAQ#why-cant-i-add-a-new-movie-when-i-know-the-tmdb-id">
+                    Why can't I find my movie?
+                  </Link>
+                </div>
+              </div>
+          }
+
+          {
+            !term &&
+              <div className={styles.message}>
+                <div className={styles.helpText}>It's easy to add a new movie, just start typing the name the movie you want to add.</div>
+                <div>You can also search using TMDB ID of a movie. eg. tmdb:71663</div>
+              </div>
+          }
+
+          <div />
+        </PageContentBodyConnector>
+      </PageContent>
+    );
+  }
+}
+
+AddNewMovie.propTypes = {
+  term: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.object,
+  isAdding: PropTypes.bool.isRequired,
+  addError: PropTypes.object,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onMovieLookupChange: PropTypes.func.isRequired,
+  onClearMovieLookup: PropTypes.func.isRequired
+};
+
+export default AddNewMovie;

frontend/src/AddMovie/AddNewMovie/AddNewMovieModalContentConnector.js
@@ -1,0 +1,97 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import { setAddMovieDefault, addMovie } from 'Store/Actions/addMovieActions';
+import createDimensionsSelector from 'Store/Selectors/createDimensionsSelector';
+import selectSettings from 'Store/Selectors/selectSettings';
+import AddNewMovieModalContent from './AddNewMovieModalContent';
+
+function createMapStateToProps() {
+  return createSelector(
+    (state) => state.addMovie,
+    createDimensionsSelector(),
+    (addMovieState, dimensions) => {
+      const {
+        isAdding,
+        addError,
+        defaults
+      } = addMovieState;
+
+      const {
+        settings,
+        validationErrors,
+        validationWarnings
+      } = selectSettings(defaults, {}, addError);
+
+      return {
+        isAdding,
+        addError,
+        isSmallScreen: dimensions.isSmallScreen,
+        validationErrors,
+        validationWarnings,
+        ...settings
+      };
+    }
+  );
+}
+
+const mapDispatchToProps = {
+  setAddMovieDefault,
+  addMovie
+};
+
+class AddNewMovieModalContentConnector extends Component {
+
+  //
+  // Listeners
+
+  onInputChange = ({ name, value }) => {
+    this.props.setAddMovieDefault({ [name]: value });
+  }
+
+  onAddMoviePress = (searchForMissingEpisodes) => {
+    const {
+      tmdbId,
+      rootFolderPath,
+      monitor,
+      qualityProfileId,
+      tags
+    } = this.props;
+
+    this.props.addMovie({
+      tmdbId,
+      rootFolderPath: rootFolderPath.value,
+      monitor: monitor.value,
+      qualityProfileId: qualityProfileId.value,
+      tags: tags.value,
+      searchForMissingEpisodes
+    });
+  }
+
+  //
+  // Render
+
+  render() {
+    return (
+      <AddNewMovieModalContent
+        {...this.props}
+        onInputChange={this.onInputChange}
+        onAddMoviePress={this.onAddMoviePress}
+      />
+    );
+  }
+}
+
+AddNewMovieModalContentConnector.propTypes = {
+  tmdbId: PropTypes.number.isRequired,
+  rootFolderPath: PropTypes.object,
+  monitor: PropTypes.object.isRequired,
+  qualityProfileId: PropTypes.object,
+  tags: PropTypes.object.isRequired,
+  onModalClose: PropTypes.func.isRequired,
+  setAddMovieDefault: PropTypes.func.isRequired,
+  addMovie: PropTypes.func.isRequired
+};
+
+export default connect(createMapStateToProps, mapDispatchToProps)(AddNewMovieModalContentConnector);