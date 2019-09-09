Sonarr/Sonarr

Updated Nancy to 2.0

src/NzbDrone.Api/NzbDroneFeedModule.cs
@@ -1,8 +1,8 @@
-﻿using Nancy;
+﻿using Sonarr.Http;
 
 namespace NzbDrone.Api
 {
-    public abstract class NzbDroneFeedModule : NancyModule
+    public abstract class NzbDroneFeedModule : SonarrModule
     {
         protected NzbDroneFeedModule(string resource)
             : base("/feed/" + resource.Trim('/'))

releasarr/releasarr

src/NzbDrone.Api/NzbDroneFeedModule.cs
@@ -1,8 +1,8 @@
-﻿using Nancy;
+﻿using Radarr.Http;
 
 namespace NzbDrone.Api
 {
-    public abstract class NzbDroneFeedModule : NancyModule
+    public abstract class NzbDroneFeedModule : RadarrModule
     {
         protected NzbDroneFeedModule(string resource)
             : base("/feed/" + resource.Trim('/'))