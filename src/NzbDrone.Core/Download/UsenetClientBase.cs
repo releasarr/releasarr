Sonarr/Sonarr

Improved error message when nzb download contains an newznab error instead

src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -52,7 +52,7 @@ namespace NzbDrone.Core.Download
                 if (ex.Response.StatusCode == HttpStatusCode.NotFound)
                 {
                     _logger.Error(ex, "Downloading nzb file for episode '{0}' failed since it no longer exists ({1})", remoteEpisode.Release.Title, url);
-                    throw new ReleaseUnavailableException(remoteEpisode.Release, "Downloading torrent failed", ex);
+                    throw new ReleaseUnavailableException(remoteEpisode.Release, "Downloading nzb failed", ex);
                 }
 
                 if ((int)ex.Response.StatusCode == 429)

releasarr/releasarr

src/NzbDrone.Core/Download/UsenetClientBase.cs
@@ -53,7 +53,7 @@ namespace NzbDrone.Core.Download
                 if (ex.Response.StatusCode == HttpStatusCode.NotFound)
                 {
                     _logger.Error(ex, "Downloading nzb file for movie '{0}' failed since it no longer exists ({1})", remoteMovie.Release.Title, url);
-                    throw new ReleaseUnavailableException(remoteMovie.Release, "Downloading torrent failed", ex);
+                    throw new ReleaseUnavailableException(remoteMovie.Release, "Downloading nzb failed", ex);
                 }
 
                 if ((int)ex.Response.StatusCode == 429)