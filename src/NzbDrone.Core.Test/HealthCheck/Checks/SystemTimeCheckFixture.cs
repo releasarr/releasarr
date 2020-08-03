Sonarr/Sonarr

New: Show health warning if system time is off expected time
Closes #1422

src/NzbDrone.Core.Test/HealthCheck/Checks/SystemTimeCheckFixture.cs
@@ -1,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Common.Cloud;
+using NzbDrone.Common.Http;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class SystemTimeCheckFixture : CoreTest<SystemTimeCheck>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mocker.SetConstant<ISonarrCloudRequestBuilder>(new SonarrCloudRequestBuilder());
+        }
+
+        private void GivenServerTime(DateTime dateTime)
+        {
+            var json = new ServiceTimeResponse {DateTimeUtc = dateTime}.ToJson();
+
+            Mocker.GetMock<IHttpClient>()
+                  .Setup(s => s.Execute(It.IsAny<HttpRequest>()))
+                  .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), Encoding.ASCII.GetBytes(json)));
+        }
+
+        [Test]
+        public void should_not_return_error_when_system_time_is_close_to_server_time()
+        {
+            GivenServerTime(DateTime.UtcNow);
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_error_when_system_time_is_more_than_one_day_from_server_time()
+        {
+            GivenServerTime(DateTime.UtcNow.AddDays(2));
+
+            Subject.Check().ShouldBeError();
+            ExceptionVerification.ExpectedErrors(1);
+        }
+    }
+}

releasarr/releasarr

Co-Authored-By: Mark McDowall <markus101@users.noreply.github.com>

src/NzbDrone.Core.Test/HealthCheck/Checks/SystemTimeCheckFixture.cs
@@ -1,0 +1,49 @@
+using System;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using NzbDrone.Common.Cloud;
+using NzbDrone.Common.Http;
+using NzbDrone.Common.Serializer;
+using NzbDrone.Core.HealthCheck.Checks;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Test.Common;
+
+namespace NzbDrone.Core.Test.HealthCheck.Checks
+{
+    [TestFixture]
+    public class SystemTimeCheckFixture : CoreTest<SystemTimeCheck>
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mocker.SetConstant<IRadarrCloudRequestBuilder>(new RadarrCloudRequestBuilder());
+        }
+
+        private void GivenServerTime(DateTime dateTime)
+        {
+            var json = new ServiceTimeResponse { DateTimeUtc = dateTime }.ToJson();
+
+            Mocker.GetMock<IHttpClient>()
+                  .Setup(s => s.Execute(It.IsAny<HttpRequest>()))
+                  .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), Encoding.ASCII.GetBytes(json)));
+        }
+
+        [Test]
+        public void should_not_return_error_when_system_time_is_close_to_server_time()
+        {
+            GivenServerTime(DateTime.UtcNow);
+
+            Subject.Check().ShouldBeOk();
+        }
+
+        [Test]
+        public void should_return_error_when_system_time_is_more_than_one_day_from_server_time()
+        {
+            GivenServerTime(DateTime.UtcNow.AddDays(2));
+
+            Subject.Check().ShouldBeError();
+            ExceptionVerification.ExpectedErrors(1);
+        }
+    }
+}