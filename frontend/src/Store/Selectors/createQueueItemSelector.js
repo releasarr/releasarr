Sonarr/Sonarr

Don't process queue item without details

frontend/src/Store/Selectors/createQueueItemSelector.js
@@ -5,7 +5,7 @@ function createQueueItemSelector() {
     (state, { episodeId }) => episodeId,
     (state) => state.queue.details.items,
     (episodeId, details) => {
-      if (!episodeId) {
+      if (!episodeId || !details) {
         return null;
       }
 

releasarr/releasarr

(cherry picked from commit dffdd3377e198ca1ce511ec2752eb53c18c92cb3)
# Conflicts:
#	frontend/src/Store/Selectors/createQueueItemSelector.js

frontend/src/Store/Selectors/createQueueItemSelector.js
@@ -5,7 +5,7 @@ function createQueueItemSelector() {
     (state, { movieId }) => movieId,
     (state) => state.queue.details.items,
     (movieId, details) => {
-      if (!movieId) {
+      if (!movieId || !details) {
         return null;
       }
 