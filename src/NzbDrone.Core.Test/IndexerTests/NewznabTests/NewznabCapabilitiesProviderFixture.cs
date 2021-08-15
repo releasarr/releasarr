johnpyp/Prowlarr

New: Per Indexer Proxies
Fixes #281

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using System.Xml;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Core.Test.Framework;
 
@@ -14,6 +16,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
     public class NewznabCapabilitiesProviderFixture : CoreTest<NewznabCapabilitiesProvider>
     {
         private NewznabSettings _settings;
+        private IndexerDefinition _definition;
         private string _caps;
 
         [SetUp]
@@ -24,14 +27,24 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
                 BaseUrl = "http://indxer.local"
             };
 
+            _definition = new IndexerDefinition()
+            {
+                Id = 5,
+                Name = "Newznab",
+                Settings = new NewznabSettings()
+                {
+                    BaseUrl = "http://indexer.local/"
+                }
+            };
+
             _caps = ReadAllText("Files/Indexers/Newznab/newznab_caps.xml");
         }
 
         private void GivenCapsResponse(string caps)
         {
-            Mocker.GetMock<IHttpClient>()
-                .Setup(o => o.Get(It.IsAny<HttpRequest>()))
-                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), new CookieCollection(), caps));
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Setup(o => o.Get(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()))
+                .Returns<HttpRequest, IndexerDefinition>((r, d) => new HttpResponse(r, new HttpHeader(), new CookieCollection(), caps));
         }
 
         [Test]
@@ -39,11 +52,11 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps);
 
-            Subject.GetCapabilities(_settings);
-            Subject.GetCapabilities(_settings);
+            Subject.GetCapabilities(_settings, _definition);
+            Subject.GetCapabilities(_settings, _definition);
 
-            Mocker.GetMock<IHttpClient>()
-                .Verify(o => o.Get(It.IsAny<HttpRequest>()), Times.Once());
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Verify(o => o.Get(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()), Times.Once());
         }
 
         [Test]
@@ -51,7 +64,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps);
 
-            var caps = Subject.GetCapabilities(_settings);
+            var caps = Subject.GetCapabilities(_settings, _definition);
 
             caps.LimitsDefault.Value.Should().Be(25);
             caps.LimitsMax.Value.Should().Be(60);
@@ -62,7 +75,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("<limits", "<abclimits"));
 
-            var caps = Subject.GetCapabilities(_settings);
+            var caps = Subject.GetCapabilities(_settings, _definition);
 
             caps.LimitsDefault.Value.Should().Be(100);
             caps.LimitsMax.Value.Should().Be(100);
@@ -71,11 +84,11 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         [Test]
         public void should_throw_if_failed_to_get()
         {
-            Mocker.GetMock<IHttpClient>()
-                .Setup(o => o.Get(It.IsAny<HttpRequest>()))
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Setup(o => o.Get(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()))
                 .Throws<Exception>();
 
-            Assert.Throws<Exception>(() => Subject.GetCapabilities(_settings));
+            Assert.Throws<Exception>(() => Subject.GetCapabilities(_settings, _definition));
         }
 
         [Test]
@@ -83,7 +96,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("<limits", "<>"));
 
-            Assert.Throws<XmlException>(() => Subject.GetCapabilities(_settings));
+            Assert.Throws<XmlException>(() => Subject.GetCapabilities(_settings, _definition));
         }
 
         [Test]
@@ -91,7 +104,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("5030", "asdf"));
 
-            var result = Subject.GetCapabilities(_settings);
+            var result = Subject.GetCapabilities(_settings, _definition);
 
             result.Should().NotBeNull();
         }

src/NzbDrone.Core/Indexers/Definitions/Newznab/NewznabCapabilitiesProvider.cs
@@ -7,36 +7,37 @@ using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
+using NzbDrone.Core.ThingiProvider;
 
 namespace NzbDrone.Core.Indexers.Newznab
 {
     public interface INewznabCapabilitiesProvider
     {
-        IndexerCapabilities GetCapabilities(NewznabSettings settings);
+        IndexerCapabilities GetCapabilities(NewznabSettings settings, ProviderDefinition definition);
     }
 
     public class NewznabCapabilitiesProvider : INewznabCapabilitiesProvider
     {
         private readonly ICached<IndexerCapabilities> _capabilitiesCache;
-        private readonly IHttpClient _httpClient;
+        private readonly IIndexerHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public NewznabCapabilitiesProvider(ICacheManager cacheManager, IHttpClient httpClient, Logger logger)
+        public NewznabCapabilitiesProvider(ICacheManager cacheManager, IIndexerHttpClient httpClient, Logger logger)
         {
             _capabilitiesCache = cacheManager.GetCache<IndexerCapabilities>(GetType());
             _httpClient = httpClient;
             _logger = logger;
         }
 
-        public IndexerCapabilities GetCapabilities(NewznabSettings indexerSettings)
+        public IndexerCapabilities GetCapabilities(NewznabSettings indexerSettings, ProviderDefinition definition)
         {
             var key = indexerSettings.ToJson();
-            var capabilities = _capabilitiesCache.Get(key, () => FetchCapabilities(indexerSettings), TimeSpan.FromDays(7));
+            var capabilities = _capabilitiesCache.Get(key, () => FetchCapabilities(indexerSettings, definition), TimeSpan.FromDays(7));
 
             return capabilities;
         }
 
-        private IndexerCapabilities FetchCapabilities(NewznabSettings indexerSettings)
+        private IndexerCapabilities FetchCapabilities(NewznabSettings indexerSettings, ProviderDefinition definition)
         {
             var capabilities = new IndexerCapabilities();
 
@@ -54,7 +55,7 @@ namespace NzbDrone.Core.Indexers.Newznab
 
             try
             {
-                response = _httpClient.Get(request);
+                response = _httpClient.Get(request, definition);
             }
             catch (Exception ex)
             {

releasarr/releasarr

src/NzbDrone.Core.Test/IndexerTests/NewznabTests/NewznabCapabilitiesProviderFixture.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using System.Xml;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers;
 using NzbDrone.Core.Indexers.Newznab;
 using NzbDrone.Core.Test.Framework;
 
@@ -14,6 +16,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
     public class NewznabCapabilitiesProviderFixture : CoreTest<NewznabCapabilitiesProvider>
     {
         private NewznabSettings _settings;
+        private IndexerDefinition _definition;
         private string _caps;
 
         [SetUp]
@@ -24,14 +27,24 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
                 BaseUrl = "http://indxer.local"
             };
 
+            _definition = new IndexerDefinition()
+            {
+                Id = 5,
+                Name = "Newznab",
+                Settings = new NewznabSettings()
+                {
+                    BaseUrl = "http://indexer.local/"
+                }
+            };
+
             _caps = ReadAllText("Files/Indexers/Newznab/newznab_caps.xml");
         }
 
         private void GivenCapsResponse(string caps)
         {
-            Mocker.GetMock<IHttpClient>()
-                .Setup(o => o.Get(It.IsAny<HttpRequest>()))
-                .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), new CookieCollection(), caps));
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Setup(o => o.Execute(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()))
+                .Returns<HttpRequest, IndexerDefinition>((r, d) => new HttpResponse(r, new HttpHeader(), new CookieCollection(), caps));
         }
 
         [Test]
@@ -39,11 +52,11 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps);
 
-            Subject.GetCapabilities(_settings);
-            Subject.GetCapabilities(_settings);
+            Subject.GetCapabilities(_settings, _definition);
+            Subject.GetCapabilities(_settings, _definition);
 
-            Mocker.GetMock<IHttpClient>()
-                .Verify(o => o.Get(It.IsAny<HttpRequest>()), Times.Once());
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Verify(o => o.Execute(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()), Times.Once());
         }
 
         [Test]
@@ -51,7 +64,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps);
 
-            var caps = Subject.GetCapabilities(_settings);
+            var caps = Subject.GetCapabilities(_settings, _definition);
 
             caps.LimitsDefault.Value.Should().Be(25);
             caps.LimitsMax.Value.Should().Be(60);
@@ -62,7 +75,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("<limits", "<abclimits"));
 
-            var caps = Subject.GetCapabilities(_settings);
+            var caps = Subject.GetCapabilities(_settings, _definition);
 
             caps.LimitsDefault.Value.Should().Be(100);
             caps.LimitsMax.Value.Should().Be(100);
@@ -71,11 +84,11 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         [Test]
         public void should_throw_if_failed_to_get()
         {
-            Mocker.GetMock<IHttpClient>()
-                .Setup(o => o.Get(It.IsAny<HttpRequest>()))
+            Mocker.GetMock<IIndexerHttpClient>()
+                .Setup(o => o.Execute(It.IsAny<HttpRequest>(), It.IsAny<IndexerDefinition>()))
                 .Throws<Exception>();
 
-            Assert.Throws<Exception>(() => Subject.GetCapabilities(_settings));
+            Assert.Throws<Exception>(() => Subject.GetCapabilities(_settings, _definition));
         }
 
         [Test]
@@ -83,7 +96,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("<limits", "<>"));
 
-            Assert.Throws<XmlException>(() => Subject.GetCapabilities(_settings));
+            Assert.Throws<XmlException>(() => Subject.GetCapabilities(_settings, _definition));
         }
 
         [Test]
@@ -91,7 +104,7 @@ namespace NzbDrone.Core.Test.IndexerTests.NewznabTests
         {
             GivenCapsResponse(_caps.Replace("5030", "asdf"));
 
-            var result = Subject.GetCapabilities(_settings);
+            var result = Subject.GetCapabilities(_settings, _definition);
 
             result.Should().NotBeNull();
         }

src/NzbDrone.Core/Indexers/Definitions/Newznab/NewznabCapabilitiesProvider.cs
@@ -7,36 +7,37 @@ using NzbDrone.Common.Cache;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
+using NzbDrone.Core.ThingiProvider;
 
 namespace NzbDrone.Core.Indexers.Newznab
 {
     public interface INewznabCapabilitiesProvider
     {
-        IndexerCapabilities GetCapabilities(NewznabSettings settings);
+        IndexerCapabilities GetCapabilities(NewznabSettings settings, ProviderDefinition definition);
     }
 
     public class NewznabCapabilitiesProvider : INewznabCapabilitiesProvider
     {
         private readonly ICached<IndexerCapabilities> _capabilitiesCache;
-        private readonly IHttpClient _httpClient;
+        private readonly IIndexerHttpClient _httpClient;
         private readonly Logger _logger;
 
-        public NewznabCapabilitiesProvider(ICacheManager cacheManager, IHttpClient httpClient, Logger logger)
+        public NewznabCapabilitiesProvider(ICacheManager cacheManager, IIndexerHttpClient httpClient, Logger logger)
         {
             _capabilitiesCache = cacheManager.GetCache<IndexerCapabilities>(GetType());
             _httpClient = httpClient;
             _logger = logger;
         }
 
-        public IndexerCapabilities GetCapabilities(NewznabSettings indexerSettings)
+        public IndexerCapabilities GetCapabilities(NewznabSettings indexerSettings, ProviderDefinition definition)
         {
             var key = indexerSettings.ToJson();
-            var capabilities = _capabilitiesCache.Get(key, () => FetchCapabilities(indexerSettings), TimeSpan.FromDays(7));
+            var capabilities = _capabilitiesCache.Get(key, () => FetchCapabilities(indexerSettings, definition), TimeSpan.FromDays(7));
 
             return capabilities;
         }
 
-        private IndexerCapabilities FetchCapabilities(NewznabSettings indexerSettings)
+        private IndexerCapabilities FetchCapabilities(NewznabSettings indexerSettings, ProviderDefinition definition)
         {
             var capabilities = new IndexerCapabilities();
 
@@ -49,12 +50,13 @@ namespace NzbDrone.Core.Indexers.Newznab
 
             var request = new HttpRequest(url, HttpAccept.Rss);
             request.AllowAutoRedirect = true;
+            request.Method = HttpMethod.GET;
 
             HttpResponse response;
 
             try
             {
-                response = _httpClient.Get(request);
+                response = _httpClient.Execute(request, definition);
             }
             catch (Exception ex)
             {