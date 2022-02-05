Prowlarr/Prowlarr

New: (Cardigann) - Cardigann v4 Support for categorydesc
based on Jackett 02e43bd6a9838562a14c6bd6ea10cfed5a293d59
based on Jackett 60d2c425e1038a28f54bda658a9f543f3a197a66

Fixes #782

src/NzbDrone.Core/Indexers/Definitions/Cardigann/CardigannParser.cs
@@ -466,6 +466,22 @@ namespace NzbDrone.Core.Indexers.Cardigann
 
                     value = release.Categories.ToString();
                     break;
+                case "categorydesc":
+                    var catsDesc = MapTrackerCatDescToNewznab(value);
+                    if (catsDesc.Any())
+                    {
+                        if (release.Category == null || fieldModifiers.Contains("noappend"))
+                        {
+                            release.Category = catsDesc;
+                        }
+                        else
+                        {
+                            release.Category = release.Category.Union(catsDesc).ToList();
+                        }
+                    }
+
+                    value = release.Category.ToString();
+                    break;
                 case "size":
                     release.Size = ParseUtil.GetBytes(value);
                     value = release.Size.ToString();

releasarr/releasarr

src/NzbDrone.Core/Indexers/Definitions/Cardigann/CardigannParser.cs
@@ -466,6 +466,22 @@ namespace NzbDrone.Core.Indexers.Cardigann
 
                     value = release.Categories.ToString();
                     break;
+                case "categorydesc":
+                    var catsDesc = MapTrackerCatDescToNewznab(value);
+                    if (catsDesc.Any())
+                    {
+                        if (release.Categories == null || fieldModifiers.Contains("noappend"))
+                        {
+                            release.Categories = catsDesc;
+                        }
+                        else
+                        {
+                            release.Categories = release.Categories.Union(catsDesc).ToList();
+                        }
+                    }
+
+                    value = release.Categories.ToString();
+                    break;
                 case "size":
                     release.Size = ParseUtil.GetBytes(value);
                     value = release.Size.ToString();