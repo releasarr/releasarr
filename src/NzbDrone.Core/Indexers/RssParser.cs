Radarr/Radarr

New: Use languages from Torznab/Newznab attributes if given

src/NzbDrone.Core/Indexers/RssParser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -12,6 +12,7 @@ using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Instrumentation;
 using NzbDrone.Core.Indexers.Exceptions;
+using NzbDrone.Core.Languages;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.Indexers
@@ -160,6 +161,7 @@ namespace NzbDrone.Core.Indexers
             releaseInfo.DownloadUrl = GetDownloadUrl(item);
             releaseInfo.InfoUrl = GetInfoUrl(item);
             releaseInfo.CommentUrl = GetCommentUrl(item);
+            releaseInfo.Languages = GetLanguages(item);
 
             try
             {
@@ -224,6 +226,11 @@ namespace NzbDrone.Core.Indexers
         protected virtual string GetCommentUrl(XElement item)
         {
             return ParseUrl((string)item.Element("comments"));
+        }
+
+        protected virtual List<Language> GetLanguages(XElement item)
+        {
+            return new List<Language>();
         }
 
         protected virtual long GetSize(XElement item)

releasarr/releasarr

(cherry picked from commit 9c5a07f62a6e32832c10c80813cd3b98c5859989)

src/NzbDrone.Core/Indexers/RssParser.cs
@@ -166,6 +166,7 @@ namespace NzbDrone.Core.Indexers
             releaseInfo.InfoUrl = GetInfoUrl(item);
             releaseInfo.CommentUrl = GetCommentUrl(item);
             releaseInfo.Categories = GetCategory(item);
+            releaseInfo.Languages = GetLanguages(item);
 
             try
             {
@@ -235,6 +236,11 @@ namespace NzbDrone.Core.Indexers
         protected virtual string GetCommentUrl(XElement item)
         {
             return ParseUrl((string)item.Element("comments"));
+        }
+
+        protected virtual List<string> GetLanguages(XElement item)
+        {
+            return new List<string>();
         }
 
         protected virtual long GetSize(XElement item)